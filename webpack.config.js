716087f27dbd4949c1fb8acdbfc5abb23a7dfc9c
Erik Bumbulis <erikb@ada.support>
2020-07-06 13:58:56 -0400

    Add files for a bare-bones widget

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules\//,
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    })
+  ],
+  resolve: {},
+}
